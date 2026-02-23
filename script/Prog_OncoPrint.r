241aff351748d2a7891eb8ffc1fab80cd9307c8f
Farnoosh Babazadeh <farnooshbabazadeh@gmail.com>
2026-02-23 15:22:03 -0500

    update oncoprint figures and barplots with sig p-values

--- script/Prog_OncoPrint.r
+++ script/Prog_OncoPrint.r
@@ -104,70 +104,86 @@ table(mut_fixed)
 ####################################################
 ## OncoPrint ---> no clinbical metadata
 ####################################################
-combo_levels <- c("Amplification;SNV/Indel", "Fusion;Amplification;SNV/Indel", "Fusion;Amplification")
+#combo_levels <- c("Amplification;SNV/Indel", "Fusion;Amplification;SNV/Indel", "Fusion;Amplification")
 
-mut_other <- mut_fixed
-mut_other[mut_other %in% combo_levels] <- "Multi-hit"
+#mut_other <- mut_fixed
+#mut_other[mut_other %in% combo_levels] <- "Multi-hit"
 
 col <- c(
   "SNV/Indel"     = "#68855CFF",
   "Amplification" = "#A06177FF",
   "Fusion"        = "#A5693CFF",
-  "Deletion"      = "#526A83FF", 
-  "Multi-hit"     = "#D9AF6BFF"
+  "Deletion"      = "#526A83FF"
+ # "Multi-hit"     = "#D9AF6BFF"
 )
 
 alter_fun <- list(
+  
   background = function(x, y, w, h) {
-    grid.rect(x, y, w - unit(2, "pt"), h - unit(2, "pt"),
-              gp = gpar(fill = "#CCCCCC", col = NA))
+  grid.rect(x, y, w, h,
+            gp = gpar(fill = NA, col = NA))
   },
+
   "SNV/Indel" = function(x, y, w, h) {
-  grid.rect(x, y, w - unit(2, "pt"), h - unit(2, "pt"),
-            gp = gpar(fill = col["SNV/Indel"], col = NA))
-},
+    grid.rect(x, y + h*0.30, w*0.95, h*0.25,
+              gp = gpar(fill = col["SNV/Indel"], col = NA))
+  },
+
   "Amplification" = function(x, y, w, h) {
-    grid.rect(x, y, w - unit(2, "pt"), h - unit(2, "pt"),
+    grid.rect(x, y + h*0.05, w*0.95, h*0.25,
               gp = gpar(fill = col["Amplification"], col = NA))
   },
+
   "Fusion" = function(x, y, w, h) {
-    grid.rect(x, y, w - unit(2, "pt"), h - unit(2, "pt"),
+    grid.rect(x, y - h*0.20, w*0.95, h*0.25,
               gp = gpar(fill = col["Fusion"], col = NA))
   },
+
   "Deletion" = function(x, y, w, h) {
-    grid.rect(x, y, w - unit(2, "pt"), h - unit(2, "pt"),
+    grid.rect(x, y - h*0.45, w*0.95, h*0.25,
               gp = gpar(fill = col["Deletion"], col = NA))
-  },
-  "Multi-hit" = function(x, y, w, h) {
-    grid.rect(x, y, w - unit(2, "pt"), h - unit(2, "pt"),
-              gp = gpar(fill = col["Multi-hit"], col = NA))
   }
 )
 
+left_annotation = rowAnnotation(
+  rbar = anno_oncoprint_barplot(
+    axis_param = list(direction = "reverse"),
+    border = FALSE
+  )
+)
+
 heatmap_legend_param <- list(
-  title  = "Alterations",
-  at     = c("SNV/Indel", "Amplification", "Fusion", "Deletion", "Multi-hit"),
-  labels = c("SNV/Indel", "Amplification", "Fusion", "Deletion", "Multi-hit")
+  title = "Alterations",
+  at = names(col),
+  labels = names(col),
+  nrow = 4,                             
+  grid_width = unit(3, "mm"),             
+  grid_height = unit(3, "mm"),
+  title_gp = gpar(fontsize = 9),          
+  labels_gp = gpar(fontsize = 7)          
 )
 
 p <- oncoPrint(
-  mut_other,
+  mut_fixed,
+  get_type = function(x) strsplit(x, ";")[[1]],
   alter_fun = alter_fun,
   col = col,
   remove_empty_rows = TRUE,
   remove_empty_columns = TRUE,
   heatmap_legend_param = heatmap_legend_param,
-    row_names_gp = grid::gpar(fontsize = 7),
-  left_annotation = rowAnnotation(
-    rbar = anno_oncoprint_barplot(
-      axis_param = list(direction = "reverse")
-    )
-  ),
+  row_names_gp = gpar(fontsize = 7),
+  left_annotation = left_annotation,
   right_annotation = NULL
 )
 
-pdf(file.path(dir_output, 'fig1.pdf'),  width = 5, height = 7)
-p
+pdf(file.path(dir_output, 'fig1.pdf'),  width = 5, height = 6)
+
+draw(
+  p,
+  heatmap_legend_side = "right",
+  annotation_legend_side = "right"
+)
+
 dev.off()
 
 ####################################################
@@ -201,7 +217,8 @@ clin <- clin %>%
 clin <- clin %>%
   mutate(
     Grade = case_when(
-      WHO.2021.Grade %in% c(1, 2) ~ "I/II",
+      WHO.2021.Grade == 1 ~ "I",
+      WHO.2021.Grade == 2 ~ "II",
       WHO.2021.Grade == 3 ~ "III",
       WHO.2021.Grade == 4 ~ "IV",
       TRUE ~ NA_character_
@@ -256,11 +273,11 @@ anno_df <- data.frame(
   IDH = factor(clin$IDH.status, levels = c("WT", "Mut", "Unknown")),
   Location = factor(clin$Location, levels = c('Lobar', 'Cerebellum', 'Thalamic', 'Other')),
   Histo = factor(clin$Histo, levels = c('Glioblastoma', 'Astrocytoma', 'Oligodendroglioma', 'Glioneuronal')),
-  Grade = factor(clin$Grade, levels = c('I/II', 'III', 'IV'))
+  Grade = factor(clin$Grade, levels = c('I', 'II', 'III', 'IV'))
 )
 
 # make sure rownames match sample IDs
-rownames(anno_df) <- colnames(mut_other)
+rownames(anno_df) <- colnames(mut_fixed)
 
 # colors
 anno_col <- list(
@@ -278,7 +295,8 @@ anno_col <- list(
     '<40' = "#C6DBEF"
   ),
   Grade = c(
-    'I/II'  = "#BC8E7DFF",
+    'I'  = "#A8C3A0FF",
+    'II'  = "#BC8E7DFF",
     'III' = "#FAE093FF",
     'IV' = "#7C7189FF"
   ),
@@ -298,19 +316,36 @@ anno_col <- list(
 )
 
 # create annotation
-ha_meta <- HeatmapAnnotation(
+top_anno <- HeatmapAnnotation(
+  
+  # ---- alteration barplot ----
+  Alterations = anno_oncoprint_barplot(
+    border = FALSE,
+    height = unit(1.2, "cm")
+  ),
+
+  # ---- metadata ----
   df = anno_df,
   col = anno_col,
-  annotation_name_side = "left"
+  simple_anno_size = unit(3, "mm"), 
+  annotation_name_side = "left",
+  annotation_name_gp = gpar(fontsize = 7) 
 )
 
-ha_bar <- HeatmapAnnotation(
-  "Alterations" = anno_oncoprint_barplot()
+heatmap_legend_param <- list(
+  title = "Alterations",
+  at = names(col),
+  labels = names(col),
+  nrow = 4,                             
+  grid_width = unit(3, "mm"),             
+  grid_height = unit(3, "mm"),
+  title_gp = gpar(fontsize = 10, face='bold'),          
+  labels_gp = gpar(fontsize = 8)          
 )
 
-top_anno <- c(ha_bar, ha_meta)
 p <- oncoPrint(
-  mut_other,
+  mut_fixed,
+  get_type = function(x) strsplit(x, ";")[[1]],
   alter_fun = alter_fun,
   col = col,
   remove_empty_rows = TRUE,
@@ -324,88 +359,22 @@ p <- oncoPrint(
   right_annotation = NULL
 )
 
-pdf(file.path(dir_output, 'fig2.pdf'), width = 8, height = 9)
-p
-dev.off()
+pdf(file.path(dir_output, 'fig2.pdf'), width = 7, height = 9)
 
+draw(
+  p,
+  heatmap_legend_side = "right",
+  annotation_legend_side = "right"
+)
+
+dev.off()
 
 ####################################################
 ## OncoPrint ---> clinbical metadata (IDH WT)
 ####################################################
 # sample-level metadata
 clin <- as.data.frame(clin)
-clin_wt <- clin[clin$IDH.status == 'IDHwt', ]
-clin_wt$Age <- ifelse(clin_wt$Age >= 40, '>40', '<40')
-
-# primary location
-clin_wt <- clin_wt %>%
-  mutate(
-    Primary.location = str_squish(Primary.location),
-    Primary.location = str_to_title(Primary.location)
-  )
-
-clin_wt <- clin_wt %>%
-  mutate(
-    Location = case_when(
-      Primary.location == "Lobar" ~ "Lobar",
-      Primary.location == "Cerebellum" ~ "Cerebellum",
-      Primary.location == "Thalamic" ~ "Thalamic",
-      TRUE ~ "Other"
-    )
-  )
-
-# Grade
-clin_wt <- clin_wt %>%
-  mutate(
-    Grade = case_when(
-      WHO.2021.Grade %in% c(1, 2) ~ "I/II",
-      WHO.2021.Grade == 3 ~ "III",
-      WHO.2021.Grade == 4 ~ "IV",
-      TRUE ~ NA_character_
-    )
-  )
-
-# Histology
-clin_wt$Histo <- coalesce(clin_wt$LGG, clin_wt$HGG)
-clin_wt$Histo <- str_squish(clin_wt$Histo)   # remove extra spaces
-clin_wt$Histo <- str_to_sentence(clin_wt$Histo)
-clin_wt <- clin_wt %>%
-  mutate(
-    Histo = str_squish(Histo),
-    Histo = str_to_sentence(Histo),
-    
-    Histo = case_when(
-      str_detect(Histo, "Glioblastoma") ~ "Glioblastoma",
-      
-      str_detect(Histo, "Diffuse hemispheric") ~ "Diffuse hemispheric",
-      str_detect(Histo, "Diffuse midline") ~ "Diffuse midline",
-      str_detect(Histo, "Diffuse high") ~ "Diffuse high-grade",
-      str_detect(Histo, "Diffuse low") ~ "Diffuse low-grade",
-      
-      str_detect(Histo, "Astrocytoma") ~ "Astrocytoma",
-      str_detect(Histo, "Oligodendroglioma") ~ "Oligodendroglioma",
-      str_detect(Histo, "Glioneuronal") ~ "Glioneuronal",
-      str_detect(Histo, "Pilocytic") ~ "Pilocytic astrocytoma",
-      str_detect(Histo, "Pleomorphic") ~ "Pleomorphic xanthoastrocytoma",
-      str_detect(Histo, "Ependymoma") ~ "Ependymoma",
-      
-      TRUE ~ Histo
-    )
-  )
-
-histo_counts <- table(clin_wt$Histo)
-
-# Identify small groups (<10 patients)
-small_groups <- names(histo_counts[histo_counts < 10])
-
-clin_wt <- clin_wt %>%
-  mutate(
-    Histo = ifelse(
-      Histo %in% small_groups,
-      "Other",
-      Histo
-    )
-  )
+clin_wt <- clin[clin$IDH.status == 'WT', ]
 
 anno_df <- data.frame(
   Sex = factor(clin_wt$Sex, levels = c("Male", "Female")),
@@ -417,7 +386,7 @@ anno_df <- data.frame(
 )
 
 # make sure rownames match sample IDs
-mut_wt <- mut_other[, colnames(mut_other) %in% clin_wt$Study]
+mut_wt <- mut_fixed[, colnames(mut_fixed) %in% clin_wt$Study]
 rownames(anno_df) <- colnames(mut_wt)
 
 # colors
@@ -436,7 +405,8 @@ anno_col <- list(
     '<40' = "#C6DBEF"
   ),
   Grade = c(
-    'I/II'  = "#BC8E7DFF",
+    'I'  = "#A8C3A0FF",
+    'II'  = "#BC8E7DFF",
     'III' = "#FAE093FF",
     'IV' = "#7C7189FF"
   ),
@@ -456,19 +426,36 @@ anno_col <- list(
 )
 
 # create annotation
-ha_meta <- HeatmapAnnotation(
+top_anno <- HeatmapAnnotation(
+  
+  # ---- alteration barplot ----
+  Alterations = anno_oncoprint_barplot(
+    border = FALSE,
+    height = unit(1.2, "cm")
+  ),
+
+  # ---- metadata ----
   df = anno_df,
   col = anno_col,
-  annotation_name_side = "left"
+  simple_anno_size = unit(3, "mm"), 
+  annotation_name_side = "left",
+  annotation_name_gp = gpar(fontsize = 7) 
 )
 
-ha_bar <- HeatmapAnnotation(
-  "Alterations" = anno_oncoprint_barplot()
+heatmap_legend_param <- list(
+  title = "Alterations",
+  at = names(col),
+  labels = names(col),
+  nrow = 4,                             
+  grid_width = unit(3, "mm"),             
+  grid_height = unit(3, "mm"),
+  title_gp = gpar(fontsize = 10, face='bold'),          
+  labels_gp = gpar(fontsize = 8)          
 )
 
-top_anno <- c(ha_bar, ha_meta)
 p <- oncoPrint(
   mut_wt,
+  get_type = function(x) strsplit(x, ";")[[1]],
   alter_fun = alter_fun,
   col = col,
   remove_empty_rows = TRUE,
@@ -483,60 +470,21 @@ p <- oncoPrint(
 )
 
 pdf(file.path(dir_output, 'fig3_wt.pdf'), width = 7, height = 9)
-p
-dev.off()
 
+draw(
+  p,
+  heatmap_legend_side = "right",
+  annotation_legend_side = "right"
+)
+
+dev.off()
 
 ####################################################
 ## OncoPrint ---> clinbical metadata (IDH MUT)
 ####################################################
 # sample-level metadata
 clin <- as.data.frame(clin)
-clin_mut <- clin[clin$IDH.status == 'IDHmut', ]
-clin_mut$Age <- ifelse(clin_mut$Age >= 40, '>40', '<40')
-
-# primary location
-clin_mut <- clin_mut %>%
-  mutate(
-    Primary.location = str_squish(Primary.location),
-    Primary.location = str_to_title(Primary.location)
-  )
-
-clin_mut <- clin_mut %>%
-  mutate(
-    Location = case_when(
-      Primary.location == "Lobar" ~ "Lobar",
-      Primary.location == "Cerebellum" ~ "Cerebellum",
-      TRUE ~ Location
-    )
-  )
-
-# Grade
-clin_mut <- clin_mut %>%
-  mutate(
-    Grade = case_when(
-      WHO.2021.Grade %in% c(1, 2) ~ "I/II",
-      WHO.2021.Grade == 3 ~ "III",
-      WHO.2021.Grade == 4 ~ "IV",
-      TRUE ~ NA_character_
-    )
-  )
-
-# Histology
-clin_mut$Histo <- coalesce(clin_mut$LGG, clin_mut$HGG)
-clin_mut$Histo <- str_squish(clin_mut$Histo)   # remove extra spaces
-clin_mut$Histo <- str_to_sentence(clin_mut$Histo)
-clin_mut <- clin_mut %>%
-  mutate(
-    Histo = str_squish(Histo),
-    Histo = str_to_sentence(Histo),
-    Histo = case_when(
-      str_detect(Histo, "Astrocytoma") ~ "Astrocytoma",
-      str_detect(Histo, "Oligodendroglioma") ~ "Oligodendroglioma",
-      TRUE ~ Histo
-    )
-  )
-
+clin_mut <- clin[clin$IDH.status == 'Mut', ]
 
 anno_df <- data.frame(
   Sex = factor(clin_mut$Sex, levels = c("Male", "Female")),
@@ -548,7 +496,7 @@ anno_df <- data.frame(
 )
 
 # make sure rownames match sample IDs
-mut_mut <- mut_other[, colnames(mut_other) %in% clin_mut$Study]
+mut_mut <- mut_fixed[, colnames(mut_fixed) %in% clin_mut$Study]
 rownames(anno_df) <- colnames(mut_mut)
 
 # colors
@@ -567,34 +515,57 @@ anno_col <- list(
     '<40' = "#C6DBEF"
   ),
   Grade = c(
-    'I/II'  = "#BC8E7DFF",
+    'I'  = "#A8C3A0FF",
+    'II'  = "#BC8E7DFF",
     'III' = "#FAE093FF",
     'IV' = "#7C7189FF"
   ),
   Location = c(
     'Lobar'  = "#A54B2DFF",
-    'Cerebellum' = "#577E2FFF"
+    'Cerebellum' = "#577E2FFF",
+    'Thalamic'  = "#B49696FF",
+    'Other' = "#96A5A5FF"
   ),
   Histo = c(
+    'Glioblastoma'       = "#4A7169FF",
     'Astrocytoma'        = "#735231FF",
-    'Oligodendroglioma'  = "#E3CA97FF"
+    'Oligodendroglioma'  = "#E3CA97FF",
+    'Glioneuronal'       = "#99B6BDFF",
+    'Other'              = "#96A5A5FF"
   )
 )
 
 # create annotation
-ha_meta <- HeatmapAnnotation(
+top_anno <- HeatmapAnnotation(
+  
+  # ---- alteration barplot ----
+  Alterations = anno_oncoprint_barplot(
+    border = FALSE,
+    height = unit(1.2, "cm")
+  ),
+
+  # ---- metadata ----
   df = anno_df,
   col = anno_col,
-  annotation_name_side = "left"
+  simple_anno_size = unit(3, "mm"), 
+  annotation_name_side = "left",
+  annotation_name_gp = gpar(fontsize = 7) 
 )
 
-ha_bar <- HeatmapAnnotation(
-  "Alterations" = anno_oncoprint_barplot()
+heatmap_legend_param <- list(
+  title = "Alterations",
+  at = names(col),
+  labels = names(col),
+  nrow = 4,                             
+  grid_width = unit(3, "mm"),             
+  grid_height = unit(3, "mm"),
+  title_gp = gpar(fontsize = 10, face='bold'),          
+  labels_gp = gpar(fontsize = 8)          
 )
 
-top_anno <- c(ha_bar, ha_meta)
 p <- oncoPrint(
   mut_mut,
+  get_type = function(x) strsplit(x, ";")[[1]],
   alter_fun = alter_fun,
   col = col,
   remove_empty_rows = TRUE,
@@ -608,8 +579,14 @@ p <- oncoPrint(
   right_annotation = NULL
 )
 
-pdf(file.path(dir_output, 'fig4_mut.pdf'), width = 6, height = 7)
-p
+pdf(file.path(dir_output, 'fig4_mut.pdf'), width = 6, height = 9)
+
+draw(
+  p,
+  heatmap_legend_side = "right",
+  annotation_legend_side = "right"
+)
+
 dev.off()
 
 ###################################################
@@ -617,11 +594,13 @@ dev.off()
 ###################################################
 load(file.path(dir_input, 'se_mut_bin_clin.RData'))
 mut <- assay(eset)
+clin <- as.data.frame(colData(eset))
+clin$Age <- ifelse(clin$Age >= 40, '>40', '<40')
 
 ## choose top mutations
 mut_freq <- rowMeans(mut == 1)
 
-genes <- names(mut_freq[mut_freq >= 0.10])
+genes <- names(mut_freq[mut_freq >= 0.1])
 mut_filt <- mut[genes, ]
 
 ## convert to long format
@@ -643,7 +622,7 @@ mut_long <- mut_long %>%
   )
 
 ###########################################################
-## Fishe exact test --- Age/sex and mutations
+## Fishe exact test --- Age and mutations
 ###########################################################
 age_assoc <- mut_long %>%
   group_by(Gene) %>%
@@ -653,16 +632,6 @@ age_assoc <- mut_long %>%
 
 age_assoc$fdr <- p.adjust(age_assoc$pval, method = "BH")
 write.csv(age_assoc, file = file.path(dir_output, 'mut_age.csv'))
-
-## Fishe exact test --- Sex and mutations
-sex_assoc <- mut_long %>%
-  group_by(Gene) %>%
-  summarise(
-    pval = fisher.test(table(Mutation, Sex))$p.value
-  )
-
-sex_assoc$fdr <- p.adjust(sex_assoc$pval, method = "BH")
-write.csv(sex_assoc, file = file.path(dir_output, 'mut_sex.csv'))
 
 ## bar plot age
 gene_order <- mut_long %>%
@@ -675,7 +644,21 @@ plot_age <- mut_long %>%
   group_by(Gene, Age) %>%
   summarise(freq = mean(Mutation) * 100, .groups = "drop")
 
+age_assoc <- age_assoc %>%
+  mutate(
+    sig = case_when(
+      pval < 0.001 ~ "***",
+      pval < 0.01  ~ "**",
+      pval < 0.05  ~ "*",
+      TRUE        ~ ""
+    )
+  )
+
 plot_age$Gene <- factor(plot_age$Gene, levels = gene_order)
+plot_age_sig <- plot_age %>%
+  group_by(Gene) %>%
+  summarise(y_pos = max(freq) + 5, .groups = "drop") %>%
+  left_join(age_assoc %>% select(Gene, sig), by = "Gene")
 
 pdf(file.path(dir_output, 'mut_age.pdf'), width = 7, height = 6)
 
@@ -685,12 +668,30 @@ ggplot(plot_age, aes(x = Gene, y = freq, fill = Age)) +
     values = c(">40" = "#08306B",
                "<40" = "#C6DBEF")
   ) +
+  geom_text(
+    data = plot_age_sig,
+    aes(x = Gene, y = y_pos, label = sig),
+    inherit.aes = FALSE,
+    size = 5
+  ) +
   theme_classic() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   labs(y = "Mutation frequency (%)", x = "", 
        title = " ")
 
 dev.off()
+
+###########################################################
+## Fishe exact test --- Sex and mutations
+###########################################################
+sex_assoc <- mut_long %>%
+  group_by(Gene) %>%
+  summarise(
+    pval = fisher.test(table(Mutation, Sex))$p.value
+  )
+
+sex_assoc$fdr <- p.adjust(sex_assoc$pval, method = "BH")
+write.csv(sex_assoc, file = file.path(dir_output, 'mut_sex.csv'))
 
 ## bar plot sex
 plot_sex <- mut_long %>%
